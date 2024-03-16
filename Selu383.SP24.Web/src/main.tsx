ea489ffa93d27f8e86b4fe84e168b60ef5771433
Seth Landry <slandry5279@gmail.com>
2024-03-16 18:23:15 -0500

    sethhomepagedev
    remove navbar redundancy and main.tsx change

--- Selu383.SP24.Web/src/main.tsx
+++ Selu383.SP24.Web/src/main.tsx
@@ -1,35 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/home/homepage";
 import Help from "./routes/help";
-import MainLayout from "./routes/home/navbar";
 import HotelDetails from "./routes/home/hotel-details/hoteldetails";
 import FindHotel from "./routes/home/FindHotel";
 import { CachePolicies, Provider } from "use-http";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-        children: [],
-      },
-      { path: "/find-hotel", element: <FindHotel /> },
-      { path: "/hotel-details/:foo", element: <HotelDetails /> },
-      {
-        path: "/help",
-        element: <Help />,
-      },
-    ],
-  },
+  { path: "/", element: <Home /> },
+  { path: "/find-hotel", element: <FindHotel /> },
+  { path: "/hotel-details/:foo", element: <HotelDetails /> },
+  { path: "/help", element: <Help /> },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <Provider options={{ cache: CachePolicies.NO_CACHE }}>
     <RouterProvider router={router} />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
