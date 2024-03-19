661ab7a31e000af121e3d5635b07ab53dc50e7fc
Alex Chaplain <alex.chaplain@selu.edu>
2024-03-19 12:02:06 -0500

    Login/Signup-Changes

--- Selu383.SP24.Web/src/routes/login/loginpage.tsx
+++ Selu383.SP24.Web/src/routes/login/loginpage.tsx
@@ -3,7 +3,7 @@ import './loginpage.css'; // Import CSS file for styling
 
 const Login: React.FC = () => {
   // State variables to store form input values
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   // Function to handle form submission
@@ -17,11 +17,13 @@ const Login: React.FC = () => {
     <div className="login-container">
       <h2>Log In</h2>
       <form className="login-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+      <div className="form-group">
+          <label><strong>Email Address:</strong></label>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <label><strong>Password:</strong></label>
+          <input type="password" placeholder="Password"value={password} onChange={(e) => setPassword(e.target.value)} required />
         </div>
         <button type="submit">Log In</button>
       </form>
